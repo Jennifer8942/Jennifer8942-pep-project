9714f6dfe27e99aafbc2b3db9843fb13207ea19f
Jennifer Gardner <jeng8942@yahoo.com>
2024-01-03 02:07:47 +0000

    Formating updates

--- src/main/java/Controller/SocialMediaController.java
+++ src/main/java/Controller/SocialMediaController.java
@@ -126,31 +126,28 @@ public class SocialMediaController {
     }
 
     /*
-    ## 4: Our API should be able to retrieve all messages.
-
-    As a user, I should be able to submit a GET request on the endpoint GET localhost:8080/messages.
-
-    - The response body should contain a JSON representation of a list containing all messages retrieved from the 
-    database. It is expected for the list to simply be empty if there are no messages. The response status should 
-    always be 200, which is the default.
-    @param ctx the context object handles HTTP requests and generates responses.
+     * Handler to retrieve all messages.
+     * 
+     * - The response body should contain a JSON representation of a list containing all messages retrieved from the 
+     *   database. It is expected for the list to simply be empty if there are no messages. The response status should 
+     *   always be 200, which is the default.
+     * 
+     * @param ctx the context object handles HTTP requests and generates responses.
      * @throws JsonProcessingException will be thrown if there is an issue converting JSON into an object.
-    */
+     */
     public void getAllMessagesHandler(Context ctx) throws JsonProcessingException {
         //TODO
     }
 
 
-    /*
-    ## 5: Our API should be able to retrieve a message by its ID.
-
-
-    As a user, I should be able to submit a GET request on the endpoint GET localhost:8080/messages/{message_id}.
-
-    - The response body should contain a JSON representation of the message identified by the message_id. It is expected 
-    for the response body to simply be empty if there is no such message. The response status should always be 200, which 
-    is the default.
-    @param ctx the context object handles HTTP requests and generates responses.
+    /* 
+     * Handler to retrieve a message by its ID.
+     * 
+     * - The response body should contain a JSON representation of the message identified by the message_id. It is expected 
+     *   for the response body to simply be empty if there is no such message. The response status should always be 200, 
+     *   which is the default.
+     * 
+     * @param ctx the context object handles HTTP requests and generates responses.
      * @throws JsonProcessingException will be thrown if there is an issue converting JSON into an object.
     */
     public void getMessageHandler(Context ctx) throws JsonProcessingException {
@@ -160,13 +157,15 @@ public class SocialMediaController {
 
 
     /*
-    ## 6: Our API should be able to delete a message identified by a message ID.
-
-    As a User, I should be able to submit a DELETE request on the endpoint DELETE localhost:8080/messages/{message_id}.
-
-    - The deletion of an existing message should remove an existing message from the database. If the message existed, the response body should contain the now-deleted message. The response status should be 200, which is the default.
-    - If the message did not exist, the response status should be 200, but the response body should be empty. This is because the DELETE verb is intended to be idempotent, ie, multiple calls to the DELETE endpoint should respond with the same type of response.
-    @param ctx the context object handles HTTP requests and generates responses.
+     * Handler to delete a message identified by a message ID.
+     * 
+     * - The deletion of an existing message should remove an existing message from the database. If the message existed, 
+     *   the response body should contain the now-deleted message. The response status should be 200, which is the default.
+     * - If the message did not exist, the response status should be 200, but the response body should be empty. This is 
+     *   because the DELETE verb is intended to be idempotent, ie, multiple calls to the DELETE endpoint should respond with 
+     *   the same type of response.
+     * 
+     * @param ctx the context object handles HTTP requests and generates responses.
      * @throws JsonProcessingException will be thrown if there is an issue converting JSON into an object.
      */
     public void deleteMessageHandler(Context ctx) throws JsonProcessingException {
@@ -174,26 +173,28 @@ public class SocialMediaController {
     }
 
     /*
-    ## 7: Our API should be able to update a message text identified by a message ID.
-
-    As a user, I should be able to submit a PATCH request on the endpoint PATCH localhost:8080/messages/{message_id}. The request body should contain a new message_text values to replace the message identified by message_id. The request body can not be guaranteed to contain any other information.
-
-    - The update of a message should be successful if and only if the message id already exists and the new message_text is not blank and is not over 255 characters. If the update is successful, the response body should contain the full updated message (including message_id, posted_by, message_text, and time_posted_epoch), and the response status should be 200, which is the default. The message existing on the database should have the updated message_text.
-    - If the update of the message is not successful for any reason, the response status should be 400. (Client error)
-    @param ctx the context object handles HTTP requests and generates responses.
+     * Handler to update a message text identified by a message ID. 
+     * 
+     * - The update of a message should be successful if and only if the message id already exists and the new message_text 
+     *   is not blank and is not over 255 characters. If the update is successful, the response body should contain the full 
+     *   updated message (including message_id, posted_by, message_text, and time_posted_epoch), and the response status 
+     *   should be 200, which is the default. The message existing on the database should have the updated message_text.
+     * - If the update of the message is not successful for any reason, the response status should be 400. (Client error) 
+     * 
+     * @param ctx the context object handles HTTP requests and generates responses.
      * @throws JsonProcessingException will be thrown if there is an issue converting JSON into an object.
-    */
+     */
     public void updateMessageHandler(Context ctx) throws JsonProcessingException {
         //TODO
     }
 
-    /*
-    ## 8: Our API should be able to retrieve all messages written by a particular user.
-
-    As a user, I should be able to submit a GET request on the endpoint GET localhost:8080/accounts/{account_id}/messages.
-
-    - The response body should contain a JSON representation of a list containing all messages posted by a particular user, which is retrieved from the database. It is expected for the list to simply be empty if there are no messages. The response status should always be 200, which is the default.
-    @param ctx the context object handles HTTP requests and generates responses.
+    /* 
+     * Handler retrieve all messages written by a particular user. 
+     * - The response body should contain a JSON representation of a list containing all messages posted by a particular 
+     *   user, which is retrieved from the database. It is expected for the list to simply be empty if there are no messages. 
+     *   The response status should always be 200, which is the default.
+     * 
+     * @param ctx the context object handles HTTP requests and generates responses.
      * @throws JsonProcessingException will be thrown if there is an issue converting JSON into an object.
     */
      public void getAllAccountMessagesHandler(Context ctx) throws JsonProcessingException {
